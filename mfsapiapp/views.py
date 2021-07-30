9ed83cd5a41dde7d64c2b30cb97668b696534f8e
Lupamo Anjichi <thelupamo@gmail.com>
2021-07-30 15:00:45 +0300

    mfs

--- mfsapiapp/views.py
+++ mfsapiapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET', 'POST'])
 @csrf_exempt
 def pClosest(request):
     """

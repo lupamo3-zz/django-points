25e9f5b8469f347f606bffa444b7ac4216705ac2
Lupamo Anjichi <thelupamo@gmail.com>
2021-07-30 15:51:18 +0300

    Mke changes to point serializer

--- mfsapiapp/views.py
+++ mfsapiapp/views.py
@@ -36,7 +36,5 @@ def pClosest(request):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-        # return HttpResponse(points[:K], content_type='json')
-    
     elif request.method == 'GET':
         return JsonResponse(points, safe=False)
